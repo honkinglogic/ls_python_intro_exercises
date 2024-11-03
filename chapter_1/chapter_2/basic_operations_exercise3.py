515e2ffd858189f6f30cfd6727e630242ee2ad00
honkinglogic <serenahks@gmail.com>
2024-11-03 13:53:54 -0500

    Add basic operations exercise 3

---
+++ chapter_1/chapter_2/basic_operations_exercise3.py
@@ -0,0 +1,11 @@
+#What does the following code do? Why?
+
+print('5' + '10')
+
+#This code prints 510, because '5' and '10' are both strings, so it uses string concatenation.
+
+#When used with string operands, + performs concatenation and returns a new string.
+
+#question 4: Refactor the code from the previous exercise to use coercion to print 15 instead.
+print(int('5') + int('10'))
+
