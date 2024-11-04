4ae785d336b68b104c7c55285a8bb0a2b65c7913
honkinglogic <serenahks@gmail.com>
2024-11-04 17:20:10 -0500

    Add Variables_Exercises_1_to_4.py

---
+++ chapter_1/chapter_2/chapter_3/Variables_Exercises_1_to_4.py
@@ -0,0 +1,66 @@
+#Classify the following potential non-constant variable names as idiomatic, non-idiomatic, or illegal. For the
+# non-idiomatic and illegal names, explain your choice.
+
+"""
+Idiomatic - names that follow conventions
+non-idiomatic -  valid but not idiomatic
+illegal - will raise a syntax error or do something unexpected
+
+Varial names - should be snake case
+"""
+index  # Idiomatic
+CatName # #non-idiomatic bc it's not snake case
+lazy_dog # Idiomatic, Snake Case
+quick_Fox #Non-idiomatic - it should be all one case
+1stCharacter #illegal names should begin with an Uppercase letter for PascaleCase
+operand2  #Idiomatic, because it starts with a lowercase letter, is all one case, and numbers are allowed
+BIG_NUMBER #Idiomatic, SCREAMING_SNAKE_CASE; #nonidiomatic for variables
+π #Legal, but non-idiomatic
+
+
+#Problem 2:
+
+#Classify the following potential function names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and
+# illegal names, explain your choice. Same as above, needs to be using snake_case
+
+"""
+index         #idiomatic
+CatName       #non-idiomatic 
+lazy_dog      #idiomatic
+quick_Fox     #non-idiomatic because of the uppercase F
+1stCharacter  #illegal
+operand2      #idiomatic
+BIG_NUMBER    #non-idiomatic
+π             #non-idiomatic
+"""
+
+#Problem 3: Classify the following potential constant names as idiomatic, non-idiomatic, or illegal. For the
+# non-idiomatic and illegal names, explain your choice.
+
+#Constants should use SCREAMING_SNAKE_CASE
+
+"""
+index        #non-idiomatic
+CatName      #non-idiomatic
+snake_case   #non-idiomatic should not be lowercase
+LAZY_DOG3    #idiomatic
+1ST          #illegal
+operand2     #non-idiomatic needs to be uppercase
+BIG_NUMBER   #idiomatic
+Π            #non-idiomatic
+"""
+
+#Problem 4: Classify the following potential class names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic
+# and illegal names, explain your choice.
+
+#Class names use PascaleCase / CamelCase
+
+"""
+index      #non-idiomatic
+CatName    #idiomatic
+Lazy_Dog   #non-idiomatic
+1ST        #illegal
+operand2   #non-idiomatic
+BigNumber3 #idiomatic
+Πi         #non-idiomatic
+"""
