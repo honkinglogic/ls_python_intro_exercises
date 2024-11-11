6208149e998d266e6dcd4750ec77cab02c3ee617
honkinglogic <serenahks@gmail.com>
2024-11-11 11:21:43 -0500

    Add FunctionsandMethodsEx8.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx8.py
@@ -0,0 +1,9 @@
+#Without running the following code, what do you think it will do?
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo(42, 3.141592, 2.718)
+
+#We've defined foo with two parameters, but passed the function three arguments - too many
