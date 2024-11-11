4f7962269623532659c1f9f98adb4054ce661e11
honkinglogic <serenahks@gmail.com>
2024-11-11 11:15:58 -0500

    Add FunctionsandMethodsEx6.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx6.py
@@ -0,0 +1,10 @@
+#What does the following code print?
+
+def scream(words):
+    words = words + '!!!!'
+    return
+    print(words)
+
+scream('Yipeee')
+
+#This code prints nothing because the return on line 5 terminates the program before it can print anything
