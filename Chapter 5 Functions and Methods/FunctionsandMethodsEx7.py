505d4219b5c79cfad923a39c50933eb4417d02c4
honkinglogic <serenahks@gmail.com>
2024-11-11 11:19:13 -0500

    Add FunctionsandMethodsEx7.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx7.py
@@ -0,0 +1,10 @@
+#Without running the following code, what do you think it will do?
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo('Hello')
+
+
+#It will raise an error: TypeError: foo() missing 1 required positional argument: 'qux'
