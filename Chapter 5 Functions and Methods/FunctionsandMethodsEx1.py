767e4606f24e1eb6e5b45bcfcf33451fc9fb566b
honkinglogic <serenahks@gmail.com>
2024-11-10 23:14:58 -0500

    Add FunctionsandMethodsEx1.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx1.py
@@ -0,0 +1,7 @@
+def set_foo():
+    foo = 'bar'
+
+set_foo()
+print(foo)
+
+#An Error, because foo is created inside a function, it isn't in scope when executing code that isn't part of that function
