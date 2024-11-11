783ac8071d37b0669360e3eae8b0c78d56807320
honkinglogic <serenahks@gmail.com>
2024-11-11 11:07:07 -0500

    Add FunctionsandMethodsEx4.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx4.py
@@ -0,0 +1,21 @@
+#Consider this code:
+
+def multiply_numbers(num1, num2, num3):
+    result = num1 * num2 * num3
+    return result
+
+product = multiply_numbers(2, 3, 4)
+
+
+"""
+function name               multiply_numbers (lines 3, 5)
+function arguments          (2, 3, 4) (Line 5)
+function definition         Everything on Lines 3 - 5
+function body               result = num1 * num2 * num3 (lines 4-5)
+                            return result
+function parameters         (num1, num2, num3)
+function invocation product  multiply_numbers(2, 3, 4) (line 7)
+function return value result  24
+all identifiers              multiply_numbers, num1, num2, num3, result, product
+
+"""
