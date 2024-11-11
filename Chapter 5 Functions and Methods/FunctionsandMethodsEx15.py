3eb0ca07c0df6fb337ad28c151ea480bae682019
honkinglogic <serenahks@gmail.com>
2024-11-11 12:22:34 -0500

    Add FunctionsandMethodsEx15.py

---
+++ Chapter 5 Functions and Methods/FunctionsandMethodsEx15.py
@@ -0,0 +1,22 @@
+#Using the code below, classify the identifiers as global, local, or built-in. For our purposes, you may
+# assume this code is the entire program.
+
+def multiply(left, right):
+    return left * right
+
+def get_num(prompt):
+    return float(input(prompt))
+
+first_number = get_num('Enter the first number: ')
+second_number = get_num('Enter the second number: ')
+product = multiply(first_number, second_number)
+print(f'{first_number} * {second_number} = {product}')
+
+#Global: multiply, get_num, first_num, second_number, product
+
+
+# Local: left, right, prompt
+
+#parameters are always local to the function they're defined on'
+
+#Built-in: float, input, print
