3526f26db1e512fa511315e5c636343367764b7d
honkinglogic <serenahks@gmail.com>
2024-11-12 12:39:46 -0500

    Add FlowControlEx3.py

---
+++ Chapter 6 Flow Control/FlowControlEx3.py
@@ -0,0 +1,18 @@
+#Without running the following code, what does it print? Why?
+
+def bar_code_scanner(serial):
+    match serial:
+        case '123':
+            print('Product1')
+        case '113':
+            print('Product2')
+        case '142':
+            print('Product3')
+        case _:
+            print('Product not found!')
+
+bar_code_scanner('113')
+bar_code_scanner(142)
+
+#It will print 113
+#it will say Product not found
