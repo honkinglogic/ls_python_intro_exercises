3422f19f6f92d4c51a94651e1df77f142a9931dc
honkinglogic <serenahks@gmail.com>
2024-11-14 00:05:23 -0500

    Add FlowControlEx6.py

---
+++ Chapter 6 Flow Control/FlowControlEx6.py
@@ -0,0 +1,16 @@
+#Write a function that takes a string as an argument and returns an all-caps version of the string when the string is
+# longer than 10 characters. Otherwise, it should return the original string.
+# Example: change 'hello world' to HELLO WORLD', but don't change 'goodbye'.
+
+def caps_long(string):
+    if len(string) > 10:
+        return string.upper()
+    else:
+        return string
+print(caps_long("hello world"))    #HELLO WORLD
+print(caps_long("goodbye"))        #goodbye
+print(caps_long(" "))              #' '
+print(caps_long("abcdefghij"))     # 'abcdefghij'
+
+
+
